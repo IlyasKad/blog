b5fd3467866fbf73ff1a168e71fa415d307d1b61
IlyasKad <kadyrbekov301@gmail.com>
2020-09-02 17:53:45 +0300

    change a color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: red;
+                color: blue;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
